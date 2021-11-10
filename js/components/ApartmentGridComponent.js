bbd296b5934d48ac4802f072c51c4a774ce43b78
AIVARAS STANAITIS <stanaitis.aivaras@gmail.com>
2021-11-10 09:03:40 +0200

    [6]: loading state

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -2,22 +2,40 @@ class ApartmentGridComponent {
   constructor() {
     this.htmlElement = document.createElement('div');
     this.state = {
-      apartments: []
+      apartments: [],
+      loading: false
     };
     this.init();
   }
 
 
+  fetchApartments = () => {
+    this.state.loading = true;
+    API.getApartments(this.saveApartments, this.showError);
+  }
+
+  saveApartments = (apartments) => {
+    this.state.apartments = apartments;
+    this.state.loading = false;
+    this.render();
+  }
+
+  showError = error => console.error(error);
+
+
   init = () => {
+    this.state.loading = true;
+    this.fetchApartments();
     this.htmlElement = document.createElement('div');
     this.render();
   }
 
   render = () => {
-    if (this.state.apartments.length === 0) {
-      this.htmlElement.innerHTML = `NĖRA DUOMENŲ`;
+    const { loading } = this.state;
+    if (loading) {
+      this.htmlElement.innerHTML = `siunčiama...`;
     } else {
-      this.htmlElement.innerHTML = "<pre>" + JSON.stringify(this.state.apartments) + "</pre>";
+      this.htmlElement.innerHTML = "Parsiųsta!";
     };
   }
 }  
